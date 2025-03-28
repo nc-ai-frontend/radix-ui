bf01427e40e8b0336c23c8bfb5f2ea7fdefffa6a
Minie Park <minijaypark@gmail.com>
2025-03-28 15:23:05 +0900

    git ignore에 dist 추가

--- dist/components/Checkbox.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-declare const Checkbox: {
-    ({ className, ref, ...props }: React.ComponentPropsWithRef<typeof CheckboxPrimitive.Root>): import("react/jsx-runtime").JSX.Element;
-    displayName: string;
-};
-export default Checkbox;
-//# sourceMappingURL=Checkbox.d.ts.map
